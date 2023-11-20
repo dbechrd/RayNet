15bf5c2ae855f36e44d88be18a69576c9a745da2
Dan Bechard <dbechrd@gmail.com>
2023-11-20 12:52:33 -0500

    Update Raylib to 5.0

--- lib/update_raylib.bat
+++ lib/update_raylib.bat
@@ -1,2 +1,3 @@
 @echo off
-cp C:\Users\User\Documents\Development\_libs\raylib\out\build\x64-Debug\raylib\raylib.lib C:\Users\User\Documents\Development\RayNet\lib\raylib.lib
+cp C:\Users\User\Documents\Development\_libs\raylib\projects\VS2022\build\raylib\bin\x64\Debug\raylib.lib C:\Users\User\Documents\Development\RayNet\lib\raylib_d.lib
+cp C:\Users\User\Documents\Development\_libs\raylib\projects\VS2022\build\raylib\bin\x64\Release\raylib.lib C:\Users\User\Documents\Development\RayNet\lib\raylib.lib
--- lib/update_yojimbo.bat
+++ lib/update_yojimbo.bat
@@ -1,5 +1,3 @@
 @echo off
 cp C:\Users\User\Documents\Development\_libs\yojimbo\bin\x64\Debug\yojimbo.lib C:\Users\User\Documents\Development\RayNet\lib\yojimbo_d.lib
-cp C:\Users\User\Documents\Development\_libs\yojimbo\bin\x64\Debug\yojimbo.pdb C:\Users\User\Documents\Development\RayNet\lib\yojimbo_d.pdb
 cp C:\Users\User\Documents\Development\_libs\yojimbo\bin\x64\Release\yojimbo.lib C:\Users\User\Documents\Development\RayNet\lib\yojimbo.lib
-cp C:\Users\User\Documents\Development\_libs\yojimbo\bin\x64\Release\yojimbo.pdb C:\Users\User\Documents\Development\RayNet\lib\yojimbo.pdb
